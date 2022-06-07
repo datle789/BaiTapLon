181d7dcd4ddf17fd5063043ae06715f164bde90b
Leanh <datletb789@gmail.com>
2022-06-07 23:54:41 +0700

    body3 added

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,23 +1,20 @@
 const main = {
+
     "cars":
         [
-            { "name": "KLARA S - Xanh lục", "price": "39.900.000", "img": "./assets/img/klaras.png","distance":"120km"},
-            { "name": "FELIZ - ĐỎ", "price": "24.900.000", "img": "./assets/img/feliz.png","distance":"90km" },
-            { "name": "KLARA A2 - 2011", "price": "26.900.000", "img": "./assets/img/bike-klara-a2.jpg","distance":"90km" },
-            { "name": "IMPES - Xanh đậm", "price": "14.900.000", "img": "./assets/img/impres-blue.jpg","distance":"70km" },
-            { "name": "IMPES - Đỏ", "price": "14.900.000", "img": "./assets/img/impes-red.jpg","distance":"70km" },
-            { "name": "KLARA S - Đen", "price": "39.900.000", "img": "./assets/img/klara-s-black.jpg","distance":"120km" },
-
-
-
+            { "name": "KLARA S - Xanh lục", "price": "39.900.000", "img": "./assets/img/klaras.png", "distance": "120km" },
+            { "name": "FELIZ - ĐỎ", "price": "24.900.000", "img": "./assets/img/feliz.png", "distance": "90km" },
+            { "name": "KLARA A2 - 2011", "price": "26.900.000", "img": "./assets/img/bike-klara-a2.jpg", "distance": "90km" },
+            { "name": "IMPES - Xanh đậm", "price": "14.900.000", "img": "./assets/img/impres-blue.jpg", "distance": "70km" },
+            { "name": "IMPES - Đỏ", "price": "14.900.000", "img": "./assets/img/impes-red.jpg", "distance": "70km" },
+            { "name": "KLARA S - Đen", "price": "39.900.000", "img": "./assets/img/klara-s-black.jpg", "distance": "120km" },
         ]
+
 }
 
 const parent = document.querySelector('.parent')
 
-
 main.cars.forEach((car) => {
-    console.log(car)
     let item = document.createElement('div')
     item.innerHTML = `
     <img src="${car.img}">
