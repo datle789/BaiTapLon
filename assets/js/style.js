5d0030df951bf9b0d36384ee9cfa4ec6b533e777
Leanh <datletb789@gmail.com>
2022-06-15 17:40:00 +0700

    fix name of html and fix address,detail of admin.json

--- assets/js/style.js
+++ assets/js/style.js
@@ -2,12 +2,12 @@
 
     "cars":
         [
-            { "name": "KLARA S - Xanh lục", "price": "39.900.000", "img": "./assets/img/klaras.png", "distance": "120km","address":"./assets/buyProduce/klaras-xanhluc.html","details":"./assets/productiondetail/detail-klaras-xanhluc.html"},
-            { "name": "FELIZ - ĐỎ", "price": "24.900.000", "img": "./assets/img/feliz.png", "distance": "90km","address":"./assets/buyProduce/feliz-do.html","details":"./assets/productiondetail/detail-feliz-do.html" },
+            { "name": "KLARA S - Xanh lục", "price": "39.900.000", "img": "./assets/img/klaras.png", "distance": "120km","address":"./assets/buyProduce/klaras-xanhluc.html","details":"./assets/productiondetail/detail-klaras.html"},
+            { "name": "FELIZ - ĐỎ", "price": "24.900.000", "img": "./assets/img/feliz.png", "distance": "90km","address":"./assets/buyProduce/feliz-do.html","details":"./assets/productiondetail/detail-feliz.html" },
             { "name": "KLARA A2 - 2011", "price": "26.900.000", "img": "./assets/img/bike-klara-a2.jpg", "distance": "90km","address":"./assets/buyProduce/klaraa2-2011.html","details":"./assets/productiondetail/detail-klaraa2-2011.html" },
-            { "name": "IMPES - Xanh đậm", "price": "14.900.000", "img": "./assets/img/impres-blue.jpg", "distance": "70km","address":"./assets/buyProduce/impes-xanhdam.html","details":"./assets/productiondetail/detail-impes-xanhdam.html" },
-            { "name": "IMPES - Đỏ", "price": "14.900.000", "img": "./assets/img/impes-red.jpg", "distance": "70km","address":"./assets/buyProduce/impes-do.html","details":"./assets/productiondetail/detail-impes-do.html" },
-            { "name": "KLARA S - Đen", "price": "39.900.000", "img": "./assets/img/klara-s-black.jpg", "distance": "120km","address":"./assets/buyProduce/klaras-den.html","details":"./assets/productiondetail/detail-klaras-den.html" },
+            { "name": "IMPES - Xanh đậm", "price": "14.900.000", "img": "./assets/img/impres-blue.jpg", "distance": "70km","address":"./assets/buyProduce/impes-xanhdam.html","details":"./assets/productiondetail/detail-impes.html" },
+            { "name": "IMPES - Đỏ", "price": "14.900.000", "img": "./assets/img/impes-red.jpg", "distance": "70km","address":"./assets/buyProduce/impes-do.html","details":"./assets/productiondetail/detail-impes.html" },
+            { "name": "KLARA S - Đen", "price": "39.900.000", "img": "./assets/img/klara-s-black.jpg", "distance": "120km","address":"./assets/buyProduce/klaras-den.html","details":"./assets/productiondetail/detail-klaras.html" },
         ]
 
 }
